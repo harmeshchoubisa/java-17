e65b82a311a66ceffab48d29034c4429ef0739a0
harmeshchoubisa <harmesh.choubisa@gmail.com>
2022-08-02 13:18:43 +0200

    Consumer functional interface

--- src/main/java/com/learn/functionalinterface/SampleBuiltInFunctionalInterface.java
+++ src/main/java/com/learn/functionalinterface/SampleBuiltInFunctionalInterface.java
@@ -1,6 +1,9 @@
 package com.learn.functionalinterface;
 
 import java.time.LocalDate;
+import java.util.HashMap;
+import java.util.function.BiConsumer;
+import java.util.function.Consumer;
 import java.util.function.Supplier;
 
 public class SampleBuiltInFunctionalInterface {
@@ -16,5 +19,24 @@ public class SampleBuiltInFunctionalInterface {
         Supplier<LocalDate> s2 = () -> LocalDate.now();
         System.out.println(s1.get());
         System.out.println(s2.get());
+
+        // A consumer is used when we want to do something with parameter but not return anything
+
+        // method reference
+        Consumer<String> c1 = System.out::println;
+        // lambda way
+        Consumer<String> c2 = x -> System.out.println(x);
+        c1.accept("First name");
+        c2.accept("Last name");
+
+        // Bi-consumer does the same thing except it takes two parameters
+        var map = new HashMap<String, Integer>();
+        // method reference
+        BiConsumer<String, Integer> b1 = map::put;
+        // lambda way
+        BiConsumer<String, Integer> b2 = (k,v) -> map.put(k, v);
+        b1.accept("Chicken", 7);
+        b2.accept("Chick", 1);
+        System.out.println(map);
     }
 }
