2a0c66cf0e1a019f5224fe890a82c17cad60483f
harmeshchoubisa <harmesh.choubisa@gmail.com>
2022-08-13 17:16:25 +0200

    try with resource where variable should be final

---
+++ src/main/java/com/learn/exceptions/SampleEffectiveFinal.java
@@ -0,0 +1,27 @@
+package com.learn.exceptions;
+
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+
+public class SampleEffectiveFinal {
+
+    public static void main(String[] args) throws IOException {
+
+        var writer = Files.newBufferedWriter(Path.of("/com/learn/exceptions/abc.txt"));
+        // variable used as a try with resource should be final or effectively final
+        try(writer) {
+            writer.append("Welcome to the zoo");
+        }
+        writer = null;
+
+
+
+        var anotherWriter = Files.newBufferedWriter(Path.of("/com/learn/exceptions/abc.txt"));
+        try(anotherWriter) {
+            anotherWriter.append("Welcome to the zoo");
+        }
+        anotherWriter.append("This will compile but it will fail and  throw IOException by saying stream is closed");
+    }
+
+}
